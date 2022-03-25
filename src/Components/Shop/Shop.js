592d1295872c9b0558348e89b5263d9d11e7ade6
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-03-25 21:25:18 +0600

    Header complete

---
+++ src/Components/Shop/Shop.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Shop = () => {
+    return (
+        <div className='shop-container'>
+            <div className='products-container'>
+                <h1>This is products container</h1>
+            </div>
+            <div className='cart-container'>
+                <h1>This is cart container</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
